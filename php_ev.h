33114141b20caa21e6951d0c72e1c41cfb487bdb
Ruslan Osmanov <rrosmanov@gmail.com>
2012-12-24 23:46:00 +0500

    Fix: version, prepare for next release

--- php_ev.h
+++ php_ev.h
@@ -37,7 +37,7 @@ ZEND_EXTERN_MODULE_GLOBALS(ev)
 extern zend_module_entry ev_module_entry;
 #define phpext_ev_ptr &ev_module_entry
 
-#define PHP_EV_VERSION "0.1.0"
+#define PHP_EV_VERSION "0.1.2"
 
 #endif /* PHP_EV_H */
 
