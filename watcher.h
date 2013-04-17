5d13baa39ce84b15bfcc0fce503c644e863edcb4
Ruslan Osmanov <rrosmanov@gmail.com>
2013-04-17 18:46:53 +0500

    Refact: removed inline decl

--- watcher.h
+++ watcher.h
@@ -131,7 +131,7 @@ void php_ev_set_watcher(ev_watcher *w, size_t size, zval *self, php_ev_loop *loo
 void *php_ev_new_watcher(size_t size, zval *self, php_ev_loop *loop,
 		const zend_fcall_info *pfci, const zend_fcall_info_cache *pfcc, zval *data, int priority TSRMLS_DC);
 void php_ev_stop_watcher(ev_watcher *watcher TSRMLS_DC);
-inline void php_ev_set_watcher_priority(ev_watcher *watcher, long priority TSRMLS_DC);
+void php_ev_set_watcher_priority(ev_watcher *watcher, long priority TSRMLS_DC);
 void php_ev_start_watcher(ev_watcher *watcher TSRMLS_DC);
 
 #endif /* PHP_EV_WATCHER_H */
